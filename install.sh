8b60c419a04f416d57c52eddf65d467b340fd16e
Kieran <kierandee5237@gmail.com>
2020-03-26 20:41:24 +0000

    ubuntu install script

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+apt install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools python3-venv nginx
+
+python3.6 -m venv venv
+
+source venv/bin/activate
+
+pip3 install wheel
+
+pip3 install -r requirements.txt
+pip3 install uwsgi
+deactivate
+cp cloud-computing.service /etc/systemd/system/cloud-computing.service
+
+systemctl start cloud-computing
+systemctl enable cloud-computing
+
+cp cloud-computing /etc/nginx/sites-available/cloud-computing
+ln -s /etc/nginx/sites-available/cloud-computing /etc/nginx/sites-enabled
+
+systemctl restart nginx
