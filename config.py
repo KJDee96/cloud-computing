5ef692b4685db1b88d9ff374bf5734576bbafb7f
Kieran <kierandee5237@gmail.com>
2020-04-04 17:13:16 +0100

    nginx dynamic local ip + postgres uri

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-                              'sqlite:///' + os.path.join(basedir, 'app.db')
+                              'postgresql://postgres:qwerty12345@database-1.cf0oovcnacdx.us-east-1.rds.amazonaws.com:5432/cloud'
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
