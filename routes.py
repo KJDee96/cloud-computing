9e7545dff4fbe4f848c9078e58fc330a4d98ea16
Kieran <kierandee5237@gmail.com>
2020-04-05 02:14:17 +0100

    remove edit profile

--- routes.py
+++ routes.py
@@ -65,20 +65,6 @@ def register():
     return render_template('register.html', title='Register', form=form)
 
 
-@app.route('/edit_profile', methods=['GET', 'POST'])
-@login_required
-def edit_profile():
-    form = EditProfileForm(current_user.username)
-    if form.validate_on_submit():
-        current_user.username = form.username.data
-        db.session.commit()
-        flash('Your changes have been saved.')
-        return redirect(url_for('index'))
-    elif request.method == 'GET':
-        form.username.data = current_user.username
-    return render_template('edit_profile.html', title='Edit Profile', form=form)
-
-
 @app.route('/add', methods=['GET', 'POST'])
 @login_required
 def add_image():
