a623eb60150cb9aa9a91fdef6879f6307a5e7408
Kieran <kierandee5237@gmail.com>
2020-04-06 00:41:57 +0100

    config from environment

--- config.py
+++ config.py
@@ -4,13 +4,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-                              'postgresql://postgres:qwerty12345@database-1.cf0oovcnacdx.us-east-1.rds.amazonaws.com:5432/cloud'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    BUCKET = 'us-east-1-image-bucket'
+    BUCKET = os.environ.get('BUCKET')
     BUCKET_FOLDER = 'https://' + BUCKET + '.s3.amazonaws.com/'
-    CLOUDFRONT = 'd3ukkq3hryg82h.cloudfront.net'
+    CLOUDFRONT = os.environ.get('CLOUDFRONT')
     UPLOADED_IMAGE_FOLDER = 'uploads/'
     UPLOADED_IMAGE_DEST = basedir + '/' + UPLOADED_IMAGE_FOLDER
--- install.sh
+++ install.sh
@@ -3,6 +3,27 @@
 apt -y update
 apt -y install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools python3-venv nginx awscli
 
+echo "DATABASE_URL=postgresql://imagedbuser:jM4A7X0s7mFX@us-east-1-rds.cf0oovcnacdx.us-east-1.rds.amazonaws.com:5432/uploads" >> /etc/environment
+echo "BUCKET=us-east-1-image-bucket" >> /etc/environment
+echo "CLOUDFRONT=dmwtvmrtya2b7.cloudfront.net" >> /etc/environment
+
+pip3 install git-remote-codecommit
+
+mkdir /root/.aws
+cd /root/.aws
+echo "[default]
+aws_access_key_id = AKIA5HUWTH575GDDVLUV
+aws_secret_access_key = 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" > credentials
+
+echo "[default]
+region = us-east-1" > config
+
+cd /var/www/
+
+git clone codecommit::us-east-1://cloud-computing
+
+cd cloud-computing
+
 python3.6 -m venv venv
 
 source venv/bin/activate
@@ -14,8 +35,6 @@ flask db upgrade
 
 deactivate
 
-aws configure
-
 cp cloud-computing.service /etc/systemd/system/cloud-computing.service
 
 systemctl start cloud-computing
@@ -23,5 +42,7 @@ systemctl enable cloud-computing
 
 cp cloud-computing /etc/nginx/sites-available/cloud-computing
 ln -s /etc/nginx/sites-available/cloud-computing /etc/nginx/sites-enabled
+
+rm /etc/nginx/sites-enabled/default
 
 systemctl restart nginx
